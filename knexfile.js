f8238132bd3a81d181b38478e84f250a16b87d27
Chakradhar-Malage <chessp526@gmail.com>
2026-02-19 16:55:44 +0530

    Project setup and Database connetion

---
+++ knexfile.js
@@ -0,0 +1,24 @@
+// knexfile.js
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST || 'localhost',
+      port: process.env.DB_PORT || 5432,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    },
+    migrations: {
+      directory: './migrations',
+    },
+    seeds: {
+      directory: './seeds',
+    },
+    pool: { min: 0, max: 10 },
+  },
+
+  // You can add test / production later
+};
